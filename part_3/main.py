ec643ec76d6b27dd10ff3a86c1f61abe740f17d4
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-10-27 23:05:38 +0300

    normal version

--- part_3/main.py
+++ part_3/main.py
@@ -3,20 +3,40 @@ import cv2
 import time
 
 import sys
-import os.path as osp
+
+
+def cv_median_filter(image1) -> None:
+    start = time.time()
+    new_image = cv2.medianBlur(image1, 3, None)
+    end = time.time()
+    print('median filter time: ', end - start)
+    create_two_windows(image1, new_image, 'original image', 'new image')
+
+
+def create_two_windows(image1: np.ndarray, image2: np.ndarray,
+                       title1: str = 'Left window', title2: str = 'Right window') -> None:
+    
+    cv2.namedWindow(title1, cv2.WINDOW_AUTOSIZE)
+    cv2.namedWindow(title2, cv2.WINDOW_AUTOSIZE)
+
+    cv2.moveWindow(title1, 100, 0)
+    cv2.moveWindow(title2, 705, 0)
+
+    image1 = cv2.resize(image1, dsize=(600, 600))
+    image2 = cv2.resize(image2, dsize=(600, 600))
+
+    cv2.imshow(title1, image1)
+    cv2.imshow(title2, image2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
-    print('Hello world!')
     try:
         image_path = sys.argv[1]
-   #     assert osp.isfile(image_path), '{} is not a file!'.format(image_path)
     except IndexError:
         print('path to the image is not valid! The default path was set!')
-    print('Hello world!')
     image = cv2.imread(image_path)
-    cv2.imshow('Original image', image)
-    new_image = cv2.medianBlur(image, 3, None)
-    cv2.imshow('new image', new_image)
-
-    cv2.waitKey(30000)
+    cv_median_filter(image)
+    cv2.waitKey(10000)
     cv2.destroyAllWindows()
