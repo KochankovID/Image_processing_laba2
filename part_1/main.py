67b0b266d9a8d4ed619b725edf2eb7920987f740
Ilya Kochankov <ilyakochankov@MBP-Ilya.Dlink>
2020-10-29 16:27:07 +0300

    gauss_noize_added

--- part_1/main.py
+++ part_1/main.py
@@ -11,10 +11,10 @@ from opencv_scripts.windows_manager import create_two_windows
 
 
 def get_gauss_noise(image_path: str):
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread(image_path)
 
-    gaussian_noise = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
-    cv2.randn(gaussian_noise, 128, 20)
+    gaussian_noise = np.zeros((image.shape[0], image.shape[1], image.shape[2]), dtype=np.uint8)
+    cv2.randn(gaussian_noise, (128, 128, 128), (20, 20, 20))
     gaussian_noise = (gaussian_noise * 0.5).astype(np.uint8)
     noisy = cv2.add(image, gaussian_noise)
 
@@ -23,9 +23,12 @@ def get_gauss_noise(image_path: str):
 
 
 if __name__ == "__main__":
-    image_path = sys.argv[1]
-    if not osp.isfile(image_path):
-        print('path "{}" to the image is not valid! The default path was set!'.format(image_path))
+    try:
+        image_path = sys.argv[1]
+        assert osp.isfile(image_path), '{} is not a file!'.format(image_path)
+    except:
+        print('path to the image is not valid! The default path was set!')
         image_path = './src/rechnaya_vidra_foto.jpg'
     get_gauss_noise(image_path)
+
 
