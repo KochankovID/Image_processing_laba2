ae7c02d51b19db2a356a6f58929dc452e6a39d37
Ilya Kochankov <ilyakochankov@MBP-Ilya.Dlink>
2020-10-29 16:52:19 +0300

    fix code style

--- part_3/main.py
+++ part_3/main.py
@@ -1,8 +1,11 @@
-import numpy as np
 import cv2
 import time
+import os.path as osp
 
 import sys
+sys.path.append('..')
+
+from opencv_scripts.windows_manager import create_two_windows
 
 
 def cv_median_filter(image1) -> None:
@@ -13,30 +16,13 @@ def cv_median_filter(image1) -> None:
     create_two_windows(image1, new_image, 'original image', 'new image')
 
 
-def create_two_windows(image1: np.ndarray, image2: np.ndarray,
-                       title1: str = 'Left window', title2: str = 'Right window') -> None:
-    
-    cv2.namedWindow(title1, cv2.WINDOW_AUTOSIZE)
-    cv2.namedWindow(title2, cv2.WINDOW_AUTOSIZE)
-
-    cv2.moveWindow(title1, 100, 0)
-    cv2.moveWindow(title2, 705, 0)
-
-    image1 = cv2.resize(image1, dsize=(600, 600))
-    image2 = cv2.resize(image2, dsize=(600, 600))
-
-    cv2.imshow(title1, image1)
-    cv2.imshow(title2, image2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
 if __name__ == "__main__":
     try:
         image_path = sys.argv[1]
-    except IndexError:
+        assert osp.isfile(image_path), '{} is not a file!'.format(image_path)
+    except (IndexError, AssertionError):
         print('path to the image is not valid! The default path was set!')
+        image_path = './src/google.jpg'
+
     image = cv2.imread(image_path)
     cv_median_filter(image)
-    cv2.waitKey(10000)
-    cv2.destroyAllWindows()
