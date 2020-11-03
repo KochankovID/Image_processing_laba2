0af3fc1f6c8da685cc840a7f9270a25c982f13b1
Ilya Kochankov <ilyakochankov@yandex.ru>
2020-11-03 14:42:03 +0300

    create separate function for gauss noise generator

--- part_1/main.py
+++ part_1/main.py
@@ -10,17 +10,20 @@ sys.path.append('..')
 from opencv_scripts.windows_manager import create_two_windows
 
 
-def get_gauss_noise(img_path: str):
-    image = cv2.imread(img_path)
-
-    start = time.time()
-
+def get_gauss_noise(image):
     gaussian_noise = np.zeros((image.shape[0], image.shape[1], image.shape[2]), dtype=np.uint8)
     cv2.randn(gaussian_noise, (128, 128, 128), (20, 20, 20))
     gaussian_noise = (gaussian_noise * 0.5).astype(np.uint8)
-    noisy = cv2.add(image, gaussian_noise)
+    return cv2.add(image, gaussian_noise)
 
-    end = time.time()
+
+def test_gauss_noise(img_path: str):
+    image = cv2.imread(img_path)
+
+    start = time.perf_counter()
+    noisy = get_gauss_noise(image)
+    end = time.perf_counter()
+
     print('median filter time: ', end - start)
 
     create_two_windows(image, noisy)
@@ -34,4 +37,4 @@ if __name__ == "__main__":
     except (IndexError, AssertionError):
         print('path to the image is not valid! The default path was set!')
         image_path = './src/rechnaya_vidra_foto.jpg'
-    get_gauss_noise(image_path)
+    test_gauss_noise(image_path)
