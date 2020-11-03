59c942313b53a938b02ed6f8d5071f843d15ca3d
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-11-03 15:00:43 +0300

    new version

--- part_3/main.py
+++ part_3/main.py
@@ -5,17 +5,26 @@ import time
 import sys
 
 
+def get_noise(image1):
+    gaussian_noise = np.zeros((image1.shape[0], image1.shape[1], image1.shape[2]), dtype=np.uint8)
+    cv2.randn(gaussian_noise, (128, 128, 128), (20, 20, 20))
+    gaussian_noise = (gaussian_noise * 0.5).astype(np.uint8)
+    return cv2.add(image1, gaussian_noise)
+
+
 def cv_median_filter(image1) -> None:
+    noisy = get_noise(image1)
     start = time.time()
-    new_image = cv2.medianBlur(image1, 3, None)
+    new_image = cv2.medianBlur(noisy, 3, None)
     end = time.time()
     print('median filter time: ', end - start)
-    create_two_windows(image1, new_image, 'original image', 'new image')
+    create_two_windows(noisy, new_image, 'original image', 'new image')
 
 
 def create_two_windows(image1: np.ndarray, image2: np.ndarray,
-                       title1: str = 'Left window', title2: str = 'Right window') -> None:
-    
+                       title1: str = 'Left window',
+                       title2: str = 'Right window') -> None:
+
     cv2.namedWindow(title1, cv2.WINDOW_AUTOSIZE)
     cv2.namedWindow(title2, cv2.WINDOW_AUTOSIZE)
 
@@ -38,5 +47,5 @@ if __name__ == "__main__":
         print('path to the image is not valid! The default path was set!')
     image = cv2.imread(image_path)
     cv_median_filter(image)
-    cv2.waitKey(10000)
+    cv2.waitKey(5000)
     cv2.destroyAllWindows()
